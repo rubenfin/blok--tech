df427d312856b5fe86df35d152dd4dd0a991e7b1
Cheaq <rubenkfinnerud@gmail.com>
2022-06-08 02:49:48 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,55 @@
+// express en port voor het starten van een server
+const express = require('express')
+const app = express()
+const port = 3000
+
+//package voor het gebruiken van .env
+const dotenv = require("dotenv").config();
+
+// packages die je nodig hebt voor het gebruiken van een database, mongodb.
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
+const { resourceUsage } = require('process');
+
+// middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// de template engine die ik gebruik, ejs.
+app.set("view engine", "ejs");
+
+
+
+// het maken van een database variabele zodat ik die overal kan opvragen.
+let db;
+
+
+app.get('/', async (req, res) => {
+  const options = { sort: { name: 1 } };
+  const users = await db.collection("users").find({}, options).toArray();
+  res.render('index.ejs', { users })
+})
+
+async function connectDB() {
+  const uri = process.env.DB_URI;
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    UseUnifiedTopology: true,
+  });
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+
+  console.log(process.env.TESTVAR);
+  connectDB().then(console.log("we have a connection to mongo"));
+})

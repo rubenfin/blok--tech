f42aa1797f41c6632ec398f7a912d0cb18c37df2
Cheaq <rubenkfinnerud@gmail.com>
2022-06-15 00:21:59 +0200

    added ejs so data can be seen on profile and me and 404 error page

--- server.js
+++ server.js
@@ -1,57 +1,56 @@
 // express en port voor het starten van een server
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 //package voor het gebruiken van .env
 const dotenv = require("dotenv").config();
-const path = require('path');
+const path = require("path");
 
 // packages die je nodig hebt voor het gebruiken van een database, mongodb.
 const { MongoClient } = require("mongodb");
 const { ObjectId } = require("mongodb");
 
-
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "static")))
+app.use(express.static(path.join(__dirname, "static")));
 
 // de template engine die ik gebruik, ejs.
 app.set("view engine", "ejs");
 
-
-
 // het maken van een database variabele zodat ik die overal kan opvragen.
 let db;
 
-app.get('/', (req, res) => {
-  res.send("Hallo")
-})
+app.get("/", (req, res) => {
+  res.send("Hallo");
+});
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   const options = { sort: { name: 1 } };
   const users = await db.collection("users").find({}, options).toArray();
   // RENDER PAGE
   const title = users.length == 0 ? "No movies were found" : "Movies";
-  res.render('users.ejs', { users })
-})
+  res.render("users.ejs", { users });
+});
 
 /* work in progress.
 app.get('/users/:userId', (req, res) => {
   res.redirect('/users/:userId/:name')
 }) */
 
-
-app.get('/users/:userId/:name', async (req, res) => {
+app.get("/users/:userId/:name", async (req, res) => {
   const query = { _id: ObjectId(req.params.userId) };
-  const users = await db.collection("users").findOne(query);
-  // RENDER PAGE
-  const title = users.length == 0 ? "No users were found" : "Users";
+  const user = await db.collection("users").findOne(query);
 
-  res.render('userprofile.ejs', { users })
-})
+  console.log(user);
+  res.render("userprofile.ejs", { user });
+});
+
+app.use("", (req, res, next) => {
+  res.status(404).send("Page not found");
+});
 
 async function connectDB() {
   const uri = process.env.DB_URI;
@@ -67,11 +66,9 @@ async function connectDB() {
   }
 }
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 
-  
   console.log(process.env.TESTVAR);
   connectDB().then(console.log("we have a connection to mongo"));
-})
+});
